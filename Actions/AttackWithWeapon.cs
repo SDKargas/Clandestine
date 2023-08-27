89cd059a36deb8a7a32ceb8f8052a9d18dcab4fc
SDKargas <111341611+SDKargas@users.noreply.github.com>
2023-08-27 16:58:09 +0200

    Add files via upload

---
+++ Actions/AttackWithWeapon.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Engine.Models;
+
+namespace Engine.Actions
+{
+    public class AttackWithWeapon : BaseAction, IAction
+    {
+        private readonly int _maximumDamage;
+        private readonly int _minimumDamage;
+        
+        public AttackWithWeapon(GameItem itemInUse, int minimumDamage, int maximumDamage)
+            : base(itemInUse)
+        {
+           if (itemInUse.Category != GameItem.ItemCategory.Weapon)
+           {
+                throw new ArgumentException($"{itemInUse.Name} is not a weapon.");
+           }
+
+           if (minimumDamage < 0)
+           {
+                throw new ArgumentException("minimum must be 0 or larger");
+           }
+
+           if (maximumDamage < _minimumDamage)
+            {
+                throw new ArgumentException("maximumDamage must be >= minimumDamage");
+            }
+
+           _maximumDamage = maximumDamage;
+           _minimumDamage = minimumDamage;
+        }
+
+        public void Execute(LivingEntity actor, LivingEntity target)
+        {
+            int damage = RandomNumberGenerator.NumberBetween(_minimumDamage, _maximumDamage);
+
+            string actorName = (actor is Player) ? "You" : $"The {actor.Name.ToLower()}";
+            string targetName = (target is Player) ? "You" : $"the {target.Name.ToLower()}";
+
+            if (damage == 0)
+            {
+                ReportResult($"{actor.Name} missed {target.Name.ToLower()}.");
+            }
+            else
+            {
+                ReportResult($"{actor.Name} hit {target.Name.ToLower()} for {damage} point{(damage > 1 ? "s" : "")}.");
+                target.TakeDamage(damage);
+            }
+        }      
+    }
+}
---
+++ TestEngine/Actions/TestAttackWithWeapon.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Engine.Actions;
+using Engine.Factories;
+using Engine.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace TestEngine.Actions
+{
+    [TestClass]
+    public class TestAttackWithWeapon
+    {
+        [TestMethod]
+        public void Test_Constructor_GoodParameters()
+        {
+            GameItem woodenSword = ItemFactory.CreateGameItem(1001);
+            AttackWithWeapon attackWithWeapon = new AttackWithWeapon(woodenSword, 1, 5);
+            Assert.IsNotNull(attackWithWeapon);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Test_Constructor_ItemIsNotAWeapon()
+        {
+            GameItem applePie = ItemFactory.CreateGameItem(2001);
+            // A granola bar is not a weapon.
+            // So, the constructor should throw an exception.
+            AttackWithWeapon attackWithWeapon = new AttackWithWeapon(applePie, 1, 5);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Test_Constructor_MinimumDamageLessThanZero()
+        {
+            GameItem woodenSword = ItemFactory.CreateGameItem(1001);
+            // This minimum damage value is less than 0.
+            // So, the constructor should throw an exception.
+            AttackWithWeapon attackWithWeapon = new AttackWithWeapon(woodenSword, -1, 5);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Test_Constructor_MaximumDamageLessThanMinimumDamage()
+        {
+            GameItem woodenSword = ItemFactory.CreateGameItem(1001);
+            // This maximum damage is less than the minimum damage.
+            // So, the constructor should throw an exception.
+            AttackWithWeapon attackWithWeapon = new AttackWithWeapon(woodenSword, 5, -1);
+        }
+    }
+}
